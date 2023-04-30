c49c299263b45969d2185856d5cfe3033605da7a
ZeSved <timothy.korell@gmail.com>
2023-04-30 21:16:29 +0200

    made progress

--- src/components/EditForm.tsx
+++ src/components/EditForm.tsx
@@ -1,3 +1,42 @@
-export default function EditForm() {
-	return <h1></h1>
+import styles from './styles/EditForm.module.css'
+import pensvg from '../assets/pen.svg'
+import { FormType } from './CustomForms'
+
+export default function EditForm({ formData, setFormData, index }: EditFormProps) {
+	function newQuestion() {
+		setFormData((formData) => {
+			const current = formData![index]
+			current.questionArr.push({ question: 'test' })
+
+			return formData
+		})
+	}
+
+	return (
+		<>
+			<button
+				onClick={newQuestion}
+				className={styles.open_form_question_creation_UI}>
+				<img
+					src={pensvg}
+					alt='edit form button'
+					className={styles.pen_svg_img}
+				/>
+			</button>
+		</>
+	)
+}
+
+export type QuestionType = {
+	question: string
+	// radialOptions: boolean
+	// checkboxOptions: boolean
+	// longAnswer: boolean
+	// shortAnswer: boolean
+}
+
+interface EditFormProps {
+	formData: FormType[]
+	setFormData: React.Dispatch<React.SetStateAction<FormType[] | undefined>>
+	index: number
 }
